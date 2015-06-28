7e503d07afdfe8964b84ef15beea80e5bf2521b0
renanyk <renanyk78@hotmail.com>
2015-06-28 14:22:04 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -288,7 +288,7 @@ void removeContatos(){
 	
         for(i=0;i<MAX_USERS;i++){
         if(contatos[i].status==1 && strcmp(name,contatos[i].name)==0){
-			strcpy(send_data,"");
+			strcpy(send_data,"teste");
 			sleep(1);
 			send(connected[contatos[i].id],send_data,strlen(send_data), 0);
 			closeConnection(contatos[i].id);
